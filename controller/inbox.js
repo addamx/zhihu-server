6afed4cd06795084be99b12085044317a52e8ad8
addams <addams.x@infinitytechnologies.com.cn>
2018-02-20 19:25:55 +0800

    v

--- controller/inbox.js
+++ controller/inbox.js
@@ -4,18 +4,30 @@ const async = require('async');
 const Inbox = model.inbox;
 
 
-module.exports = function(app) {
-  app.get('/inbox/all', (req,res) => {
+module.exports = function (app) {
+  app.get('/inbox/all', (req, res) => {
     const userId = req.decoded.id;
-    
-    Inbox.findOne({chatId: {$regex: userId}})   //new RegExp(userId)
-      .populate({path: 'messageList', populate:{path:'from', select:'name'}, model: 'inbox'})
+
+    Inbox.find({
+        chatId: {
+          $regex: userId
+        }
+      }) //new RegExp(userId)
+      .populate({
+        // "array.field_name" 填充数组内的字段
+        path: 'messageList.from messageList.to',
+        select: 'name'
+      })
       .exec((err, doc) => {
         if (err) {
-          console.log(err.message)
-          return res.status(500).json({ msg: '服务器出错' });
+          return res.status(500).json({
+            msg: '服务器出错'
+          });
         }
-        return res.status(200).json({ code: 0, data: doc });
+        return res.status(200).json({
+          code: 0,
+          data: doc
+        });
       })
   })
 }
